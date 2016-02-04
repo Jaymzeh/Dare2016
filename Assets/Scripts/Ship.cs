99d6e6919ade7a8dc4baf36c6ced5e268acaeb3a
James Smith <james_smith.games@live.co.uk>
2016-02-04 17:08:54 +0000

    Created Windows build.

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class Ship : MonoBehaviour {
 
-    public float floatHeight;
     public float amplitude = 1;
     public float speed = 1;
     public float rotateSpeed;
@@ -15,25 +14,20 @@ public class Ship : MonoBehaviour {
 	void Start () {
         y0 = transform.position.y;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        ray = new Ray(transform.position, Vector3.down * floatHeight);
+
+    // Update is called once per frame
+    void Update() {
+        ray = new Ray(transform.position, Vector3.down);
         if (Physics.Raycast(ray, out rayHit, groundLayer)) {
             y0 = rayHit.point.y + 1;
         }
         transform.position = new Vector3(transform.position.x,
         y0 + amplitude * Mathf.Sin(speed * Time.time),
         transform.position.z);
-
-
         transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime, Space.World);
 
-          
-
-            transform.localPosition += Input.GetAxis("Vertical")*transform.forward;
-        
-	}
+        transform.localPosition += Input.GetAxis("Vertical") * transform.forward;
+    }
 
     void OnDrawGizmos() {
         //Gizmos.DrawRay(ray);
